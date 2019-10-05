69e677395a7d85be2a15406e798e0a3abdbafc29
hackett91 <C16723031@mydit.ie>
2019-10-05 22:58:31 +0100

    first commit

---
+++ Lab1/problem_1/List.js
@@ -0,0 +1,22 @@
+var form = document.querySelector('form');
+form.addEventListener('submit', insertEvent);
+
+function insertEvent (event) {
+    console.log(event);
+    event.preventDefault();
+    addListElement(readFormInput('fullName'));
+}
+
+function readFormInput (data) {
+    var inputData = {};
+    inputData[data] = document.getElementById(data).value;
+    return inputData;
+}
+
+function addListElement (data) {
+    var liNode = document.createElement("li");
+    var textNode = document.createTextNode(data.fullName);
+    liNode.appendChild(textNode);
+    liNode.setAttribute("id", "card");
+    document.getElementById("nameList").appendChild(liNode);
+}
